b5e6b4ed1f9332bd96d2b7c849bacc297607c576
zhouy <rememberber@163.com>
2018-02-06 19:07:10 +0800

    支持每条消息自定义内容-初步完毕

--- src/main/java/com/fangxuele/tool/wechat/push/logic/KeFuMsgServiceThread.java
+++ src/main/java/com/fangxuele/tool/wechat/push/logic/KeFuMsgServiceThread.java
@@ -26,7 +26,7 @@ public class KeFuMsgServiceThread extends BaseMsgServiceThread {
         // 初始化当前线程
         initCurrentThread();
 
-        WxMpKefuMessage wxMpKefuMessage = PushManage.makeKefuMessage();
+        WxMpKefuMessage wxMpKefuMessage;
 
         for (int i = 0; i < list.size(); i++) {
             if (!PushData.running) {
@@ -35,8 +35,11 @@ public class KeFuMsgServiceThread extends BaseMsgServiceThread {
                 return;
             }
 
-            String openId = list.get(i)[0];
+            // 本条消息所需的数据
+            String[] msgData = list.get(i);
+            String openId = msgData[0];
             try {
+                wxMpKefuMessage = PushManage.makeKefuMessage(msgData);
                 wxMpKefuMessage.setToUser(openId);
 
                 // 空跑控制
@@ -53,14 +56,14 @@ public class KeFuMsgServiceThread extends BaseMsgServiceThread {
                 tableModel.setValueAt(currentThreadSuccessCount, tableRow, 2);
 
                 // 保存发送成功
-                PushData.sendSuccessList.add(list.get(i));
+                PushData.sendSuccessList.add(msgData);
             } catch (Exception e) {
                 // 总发送失败+1
                 PushData.increaseFail();
                 MainWindow.mainWindow.getPushFailCount().setText(String.valueOf(PushData.failRecords));
 
                 // 保存发送失败
-                PushData.sendFailList.add(list.get(i));
+                PushData.sendFailList.add(msgData);
 
                 // 失败异常信息输出控制台
                 PushManage.console(new StringBuffer().append("发送失败:").append(e.getMessage()).append(";openid:").append(openId).toString());
