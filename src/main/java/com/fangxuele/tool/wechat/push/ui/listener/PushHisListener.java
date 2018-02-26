fae79849926832e96c69564708beee9b4e94f166
zhouy <rememberber@163.com>
2018-02-26 18:07:19 +0800

    推送历史导出功能完毕

--- src/main/java/com/fangxuele/tool/wechat/push/ui/listener/PushHisListener.java
+++ src/main/java/com/fangxuele/tool/wechat/push/ui/listener/PushHisListener.java
@@ -3,6 +3,7 @@ package com.fangxuele.tool.wechat.push.ui.listener;
 import com.fangxuele.tool.wechat.push.ui.Init;
 import com.fangxuele.tool.wechat.push.ui.MainWindow;
 import com.fangxuele.tool.wechat.push.util.SystemUtil;
+import com.xiaoleilu.hutool.io.FileUtil;
 import com.xiaoleilu.hutool.log.Log;
 import com.xiaoleilu.hutool.log.LogFactory;
 import com.xiaoleilu.hutool.util.ClipboardUtil;
@@ -10,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -142,9 +144,49 @@ public class PushHisListener {
         MainWindow.mainWindow.getPushHisExportButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO 导出推送历史
-                JOptionPane.showMessageDialog(MainWindow.mainWindow.getSettingPanel(), "敬请期待！", "提示",
-                        JOptionPane.INFORMATION_MESSAGE);
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int approve = fileChooser.showOpenDialog(MainWindow.mainWindow.getSettingPanel());
+                if (approve == JFileChooser.APPROVE_OPTION) {
+                    String exportPath = fileChooser.getSelectedFile().getAbsolutePath();
+
+                    try {
+                        DefaultTableModel tableModel = (DefaultTableModel) MainWindow.mainWindow.getPushHisLeftTable()
+                                .getModel();
+                        int rowCount = tableModel.getRowCount();
+                        for (int i = 0; i < rowCount; ) {
+                            boolean delete = (boolean) tableModel.getValueAt(i, 0);
+                            if (delete) {
+                                String fileName = (String) tableModel.getValueAt(i, 1);
+                                File msgTemplateDataFile = new File(SystemUtil.configHome + "data" + File.separator + "push_his" + File.separator + fileName);
+                                if (msgTemplateDataFile.exists()) {
+                                    FileUtil.copy(msgTemplateDataFile.getAbsolutePath(), exportPath, true);
+                                }
+                                tableModel.removeRow(i);
+                                MainWindow.mainWindow.getPushHisLeftTable().updateUI();
+                                i = 0;
+                                rowCount = tableModel.getRowCount();
+                                continue;
+                            } else {
+                                i++;
+                            }
+                        }
+
+                        JOptionPane.showMessageDialog(MainWindow.mainWindow.getSettingPanel(), "导出成功！", "提示",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        try {
+                            Desktop desktop = Desktop.getDesktop();
+                            desktop.open(new File(exportPath));
+                        } catch (Exception e2) {
+                            logger.error(e2);
+                        }
+
+                    } catch (Exception e1) {
+                        JOptionPane.showMessageDialog(MainWindow.mainWindow.getSettingPanel(), "导出失败！\n\n" + e1.getMessage(), "失败",
+                                JOptionPane.ERROR_MESSAGE);
+                        logger.error(e1);
+                    }
+                }
             }
         });
 
