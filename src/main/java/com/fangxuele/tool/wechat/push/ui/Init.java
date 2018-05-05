4c68cdf7c95847c8c4371df7949e19b44810f562
zhouy <rememberber@163.com>
2018-05-05 14:55:04 +0800

    增加小程序相关的设置项

--- src/main/java/com/fangxuele/tool/wechat/push/ui/Init.java
+++ src/main/java/com/fangxuele/tool/wechat/push/ui/Init.java
@@ -269,12 +269,12 @@ public class Init {
                 MainWindow.mainWindow.getTemplateMsgPanel().setVisible(true);
                 MainWindow.mainWindow.getTemplateUrlLabel().setVisible(true);
                 MainWindow.mainWindow.getMsgTemplateUrlTextField().setVisible(true);
-                MainWindow.mainWindow.getTemplateMiniProgramAppidLabel().setVisible(true);
-                MainWindow.mainWindow.getMsgTemplateMiniAppidTextField().setVisible(true);
-                MainWindow.mainWindow.getTemplateMiniProgramPagePathLabel().setVisible(true);
-                MainWindow.mainWindow.getMsgTemplateMiniPagePathTextField().setVisible(true);
-                MainWindow.mainWindow.getTemplateMiniProgramOptionalLabel1().setVisible(true);
-                MainWindow.mainWindow.getTemplateMiniProgramOptionalLabel2().setVisible(true);
+                MainWindow.mainWindow.getTemplateMiniProgramAppidLabel().setVisible(false);
+                MainWindow.mainWindow.getMsgTemplateMiniAppidTextField().setVisible(false);
+                MainWindow.mainWindow.getTemplateMiniProgramPagePathLabel().setVisible(false);
+                MainWindow.mainWindow.getMsgTemplateMiniPagePathTextField().setVisible(false);
+                MainWindow.mainWindow.getTemplateMiniProgramOptionalLabel1().setVisible(false);
+                MainWindow.mainWindow.getTemplateMiniProgramOptionalLabel2().setVisible(false);
                 MainWindow.mainWindow.getTemplateMsgColorLabel().setVisible(true);
                 MainWindow.mainWindow.getTemplateDataColorTextField().setVisible(true);
                 MainWindow.mainWindow.getMsgTemplateFormIdTextField().setVisible(true);
