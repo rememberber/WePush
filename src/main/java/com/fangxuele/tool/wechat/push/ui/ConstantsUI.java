63f68afe295419af2eddab5c5da452ddb26217f2
zhouy <rememberber@163.com>
2018-05-09 09:49:27 +0800

    更新版本号

--- src/main/java/com/fangxuele/tool/wechat/push/ui/ConstantsUI.java
+++ src/main/java/com/fangxuele/tool/wechat/push/ui/ConstantsUI.java
@@ -14,7 +14,7 @@ public class ConstantsUI {
      * 软件名称,版本
      */
     public final static String APP_NAME = "WePush";
-    public final static String APP_VERSION = "v_2.0.2_180401";
+    public final static String APP_VERSION = "v_2.1.0_180509";
 
     /**
      * 主窗口图标
