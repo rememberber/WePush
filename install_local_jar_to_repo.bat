48b631a331bcbf21f86ffbba194bbd85a14f6ffe
duoduo <rememberber@163.com>
2018-12-28 09:36:40 +0800

    梳理mvn install 本地jar

---
+++ install_local_jar_to_repo.bat
@@ -0,0 +1,8 @@
+mvn install:install-file -Dfile=./src/main/lib/darcula.jar -DgroupId=com.darcula -DartifactId=darcula-lnf -Dpackaging=jar -Dversion=1.0 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/beautyeye_lnf.jar -DgroupId=com.beautyeye -DartifactId=beautyeye-lnf -Dpackaging=jar -Dversion=1.0 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/taobao-sdk-java.jar -DgroupId=com.taobao -DartifactId=top-auto-sdk -Dpackaging=jar -Dversion=1.0 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/weblaf-1.29.jar -DgroupId=com.weblaf -DartifactId=weblaf-lnf -Dpackaging=jar -Dversion=1.2.9 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/antlr-2.7.4.jar -DgroupId=net.sourceforge.cpdetector -DartifactId=antlr -Dpackaging=jar -Dversion=2.7.4 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/chardet-1.0.jar -DgroupId=net.sourceforge.cpdetector -DartifactId=chardet -Dpackaging=jar -Dversion=1.0.0 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/cpdetector_1.0.10.jar -DgroupId=net.sourceforge.cpdetector -DartifactId=cpdetector -Dpackaging=jar -Dversion=1.0.10 -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=./src/main/lib/jargs-1.0.jar -DgroupId=net.sourceforge.cpdetector -DartifactId=jargs -Dpackaging=jar -Dversion=1.0.0 -DgeneratePom=true -DcreateChecksum=true
