2b9c4417c8d3a238aa8c0a45bcc94ab1af778416
rememberber <rememberber@163.com>
2019-06-24 21:48:22 +0800

    release:v_3.4.1_190624

--- src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
+++ src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
@@ -16,7 +16,7 @@ public class UiConsts {
      * 软件名称,版本
      */
     public final static String APP_NAME = "WePush";
-    public final static String APP_VERSION = "v_3.4.0_190619";
+    public final static String APP_VERSION = "v_3.4.1_190624";
 
     /**
      * 主窗口图标-大
