b23472b0d576b7b358c954c68acc556ee7afe688
duoduo <rememberber@163.com>
2019-07-04 00:22:09 +0800

    feature:性能模式增加帮助提示

--- src/main/java/com/fangxuele/tool/push/ui/listener/BoostListener.java
+++ src/main/java/com/fangxuele/tool/push/ui/listener/BoostListener.java
@@ -30,7 +30,7 @@ public class BoostListener {
                 StringBuilder tipsBuilder = new StringBuilder();
                 tipsBuilder.append("<h1>什么是性能模式？</h1>");
                 tipsBuilder.append("<h2>最大限度利用系统资源，提升性能，实验性地不断优化，以期获得更快速的批量推送效果</h2>");
-                tipsBuilder.append("<p>利用异步http、NIO等技术提高批量推送效率</p>");
+                tipsBuilder.append("<p>利用异步HTTP、NIO等技术提高批量推送效率</p>");
                 tipsBuilder.append("<p>不断学习使用新技术，优化无止境，不择手段地提升批量推送速度</p>");
                 tipsBuilder.append("<p><strong>注意：性能模式下CPU、内存、网络连接资源占用过大，" +
                         "执行期间如果出现机器卡顿、浏览器无法访问等属正常现象，推送结束即可自动恢复。</strong></p>");
