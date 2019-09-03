227b9c71522dfdf935d9908cd2a9e07eda5841e3
duoduo <rememberber@163.com>
2019-09-03 18:48:19 +0800

    bug fix:修复微信小程序相关消息accessToken缓存机制问题

--- src/main/java/com/fangxuele/tool/push/logic/msgmaker/WxMaTemplateMsgMaker.java
+++ src/main/java/com/fangxuele/tool/push/logic/msgmaker/WxMaTemplateMsgMaker.java
@@ -3,7 +3,6 @@ package com.fangxuele.tool.push.logic.msgmaker;
 import cn.binarywang.wx.miniapp.bean.WxMaTemplateData;
 import cn.binarywang.wx.miniapp.bean.WxMaTemplateMessage;
 import com.fangxuele.tool.push.bean.TemplateData;
-import com.fangxuele.tool.push.logic.msgsender.WxMaTemplateMsgSender;
 import com.fangxuele.tool.push.ui.form.msg.MaTemplateMsgForm;
 import com.fangxuele.tool.push.util.TemplateUtil;
 import org.apache.commons.compress.utils.Lists;
@@ -54,8 +53,6 @@ public class WxMaTemplateMsgMaker extends BaseMsgMaker implements IMsgMaker {
             templateData.setColor(color);
             templateDataList.add(templateData);
         }
-        WxMaTemplateMsgSender.wxMaConfigStorage = null;
-        WxMaTemplateMsgSender.wxMaService = null;
     }
 
     /**
