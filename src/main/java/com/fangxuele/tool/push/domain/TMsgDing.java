0440b3bf69e5dae2a1663db59c3e2f7b1e96777c
duoduo <rememberber@163.com>
2019-09-03 22:15:00 +0800

    feature:增加钉钉消息表

---
+++ src/main/java/com/fangxuele/tool/push/domain/TMsgDing.java
@@ -0,0 +1,97 @@
+package com.fangxuele.tool.push.domain;
+
+import java.io.Serializable;
+
+public class TMsgDing implements Serializable {
+    private Integer id;
+
+    private Integer msgType;
+
+    private String msgName;
+
+    private String dingMsgType;
+
+    private String agentId;
+
+    private String webHook;
+
+    private String content;
+
+    private String createTime;
+
+    private String modifiedTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(Integer msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getMsgName() {
+        return msgName;
+    }
+
+    public void setMsgName(String msgName) {
+        this.msgName = msgName == null ? null : msgName.trim();
+    }
+
+    public String getDingMsgType() {
+        return dingMsgType;
+    }
+
+    public void setDingMsgType(String dingMsgType) {
+        this.dingMsgType = dingMsgType == null ? null : dingMsgType.trim();
+    }
+
+    public String getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(String agentId) {
+        this.agentId = agentId == null ? null : agentId.trim();
+    }
+
+    public String getWebHook() {
+        return webHook;
+    }
+
+    public void setWebHook(String webHook) {
+        this.webHook = webHook == null ? null : webHook.trim();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+
+    public String getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(String modifiedTime) {
+        this.modifiedTime = modifiedTime == null ? null : modifiedTime.trim();
+    }
+}
