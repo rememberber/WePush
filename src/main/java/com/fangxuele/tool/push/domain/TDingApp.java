cb2890a9dff09cbb7a36b311e372828088828f68
duoduo <rememberber@163.com>
2019-09-05 13:42:59 +0800

    feature:钉钉消息增加应用管理

---
+++ src/main/java/com/fangxuele/tool/push/domain/TDingApp.java
@@ -0,0 +1,77 @@
+package com.fangxuele.tool.push.domain;
+
+import java.io.Serializable;
+
+public class TDingApp implements Serializable {
+    private Integer id;
+
+    private String appName;
+
+    private String agentId;
+
+    private String appKey;
+
+    private String appSecret;
+
+    private String createTime;
+
+    private String modifiedTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName == null ? null : appName.trim();
+    }
+
+    public String getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(String agentId) {
+        this.agentId = agentId == null ? null : agentId.trim();
+    }
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public void setAppKey(String appKey) {
+        this.appKey = appKey == null ? null : appKey.trim();
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret == null ? null : appSecret.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+
+    public String getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(String modifiedTime) {
+        this.modifiedTime = modifiedTime == null ? null : modifiedTime.trim();
+    }
+}
