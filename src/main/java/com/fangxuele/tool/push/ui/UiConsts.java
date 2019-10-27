9e8733ffca85fefe0a3767315ad60d1ebcc0665f
duoduo <rememberber@163.com>
2019-10-27 14:49:42 +0800

    release:v_4.1.1_191027

--- src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
+++ src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
@@ -16,7 +16,7 @@ public class UiConsts {
      * 软件名称,版本
      */
     public final static String APP_NAME = "WePush";
-    public final static String APP_VERSION = "v_4.1.0_190930";
+    public final static String APP_VERSION = "v_4.1.1_191027";
 
     /**
      * Logo-1024*1024
