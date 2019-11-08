2e27114f7f307acf2b85643aecee997907671144
duoduo <rememberber@163.com>
2019-11-08 09:43:37 +0800

    optimization:版本更新检查资源路径改为gitee

--- src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
+++ src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
@@ -87,7 +87,7 @@ public class UiConsts {
     /**
      * 软件版本检查url
      */
-    public final static String CHECK_VERSION_URL = "https://raw.githubusercontent.com/rememberber/WePush/master/src/main/resources/version_summary.json";
+    public final static String CHECK_VERSION_URL = "https://gitee.com/zhoubochina/WePush/raw/master/src/main/resources/version_summary.json";
 
     /**
      * 用户案例url
