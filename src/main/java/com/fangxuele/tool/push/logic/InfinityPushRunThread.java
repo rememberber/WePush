0a3bf4187084359c622157c82b07fd4da01917c1
zhoubo58 <zhoubo9@jd.com>
2020-12-30 16:49:55 +0800

    新增变速模式功能初步

---
+++ src/main/java/com/fangxuele/tool/push/logic/InfinityPushRunThread.java
@@ -0,0 +1,173 @@
+package com.fangxuele.tool.push.logic;
+
+import cn.hutool.core.date.BetweenFormater;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.cron.pattern.CronPattern;
+import cn.hutool.cron.pattern.CronPatternUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import com.fangxuele.tool.push.App;
+import com.fangxuele.tool.push.logic.msgsender.IMsgSender;
+import com.fangxuele.tool.push.logic.msgsender.MsgSenderFactory;
+import com.fangxuele.tool.push.logic.msgthread.MsgAsyncSendThread;
+import com.fangxuele.tool.push.ui.form.InfinityForm;
+import com.fangxuele.tool.push.util.ConsoleUtil;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * <pre>
+ * 性能模式推送执行控制线程
+ * </pre>
+ *
+ * @author <a href="https://github.com/rememberber">RememBerBer</a>
+ * @since 2019/7/7.
+ */
+public class InfinityPushRunThread extends Thread {
+
+    private static final Log logger = LogFactory.get();
+
+    @Override
+    public void run() {
+        InfinityForm infinityForm = InfinityForm.getInstance();
+        infinityForm.getPushTotalProgressBar().setIndeterminate(true);
+        // 准备推送
+        preparePushRun();
+        infinityForm.getPushTotalProgressBar().setIndeterminate(false);
+        ConsoleUtil.boostConsoleWithLog("推送开始……");
+        // 消息数据分片以及线程纷发
+        shardingAndMsgThread();
+        // 时间监控
+        timeMonitor();
+    }
+
+    /**
+     * 准备推送
+     */
+    private void preparePushRun() {
+        InfinityForm infinityForm = InfinityForm.getInstance();
+
+        // 按钮状态
+        infinityForm.getScheduleRunButton().setEnabled(false);
+        infinityForm.getPushStartButton().setEnabled(false);
+        infinityForm.getPushStopButton().setEnabled(true);
+
+        infinityForm.getPushStopButton().setText("停止");
+        // 初始化
+        infinityForm.getPushSuccessCount().setText("0");
+        infinityForm.getPushFailCount().setText("0");
+
+        // 设置是否空跑
+        PushControl.dryRun = infinityForm.getDryRunCheckBox().isSelected();
+
+        // 执行前重新导入目标用户
+        PushControl.reimportMembers();
+
+        // 重置推送数据
+        PushData.reset();
+        PushData.startTime = System.currentTimeMillis();
+
+        // 拷贝准备的目标用户
+        PushData.toSendList.addAll(PushData.allUser);
+        PushData.TO_SEND_COUNT.set(PushData.allUser.size());
+        // 总记录数
+        PushData.totalRecords = PushData.toSendList.size();
+
+        infinityForm.getPushTotalCountLabel().setText("消息总数：" + PushData.totalRecords);
+        infinityForm.getPushTotalProgressBar().setMaximum((int) PushData.totalRecords);
+        ConsoleUtil.boostConsoleWithLog("消息总数：" + PushData.totalRecords);
+        // 可用处理器核心数量
+        infinityForm.getAvailableProcessorLabel().setText("可用处理器核心：" + Runtime.getRuntime().availableProcessors());
+        ConsoleUtil.boostConsoleWithLog("可用处理器核心：" + Runtime.getRuntime().availableProcessors());
+
+        // 准备消息构造器
+        PushControl.prepareMsgMaker();
+    }
+
+    /**
+     * 消息数据分片以及线程纷发
+     */
+    private static void shardingAndMsgThread() {
+
+        MsgAsyncSendThread msgAsyncSendThread;
+
+        IMsgSender msgSender = MsgSenderFactory.getMsgSender();
+        msgAsyncSendThread = new MsgAsyncSendThread(msgSender);
+
+        ThreadUtil.execute(msgAsyncSendThread);
+        ConsoleUtil.boostConsoleWithLog("线程启动完毕……");
+    }
+
+    /**
+     * 时间监控
+     */
+    private void timeMonitor() {
+        InfinityForm infinityForm = InfinityForm.getInstance();
+
+        long startTimeMillis = System.currentTimeMillis();
+        // 计时
+        while (true) {
+            if (PushData.TO_SEND_COUNT.get() <= PushData.successRecords.longValue() + PushData.failRecords.longValue()) {
+                if (!PushData.fixRateScheduling) {
+                    infinityForm.getPushStopButton().setEnabled(false);
+                    infinityForm.getPushStopButton().updateUI();
+                    infinityForm.getPushStartButton().setEnabled(true);
+                    infinityForm.getPushStartButton().updateUI();
+                    infinityForm.getScheduleRunButton().setEnabled(true);
+                    infinityForm.getScheduleRunButton().updateUI();
+                    infinityForm.getScheduleDetailLabel().setText("");
+                    String finishTip = "发送完毕！\n";
+                    JOptionPane.showMessageDialog(infinityForm.getInfinityPanel(), finishTip, "提示",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    infinityForm.getScheduleDetailLabel().setVisible(false);
+                } else {
+                    if (App.config.isRadioCron()) {
+                        Date nextDate = CronPatternUtil.nextDateAfter(new CronPattern(App.config.getTextCron()), new Date(), true);
+                        infinityForm.getScheduleDetailLabel().setText("计划任务执行中，下一次执行时间：" + DateFormatUtils.format(nextDate, "yyyy-MM-dd HH:mm:ss"));
+                    }
+                    infinityForm.getPushStopButton().setText("停止计划任务");
+                }
+
+                PushData.endTime = System.currentTimeMillis();
+
+                // 保存停止前的数据
+                try {
+                    // 空跑控制
+                    if (!infinityForm.getDryRunCheckBox().isSelected()) {
+                        ConsoleUtil.boostConsoleWithLog("正在保存结果数据……");
+                        infinityForm.getPushTotalProgressBar().setIndeterminate(true);
+                        PushControl.savePushData();
+                        ConsoleUtil.boostConsoleWithLog("结果数据保存完毕！");
+                    }
+                } catch (IOException e) {
+                    logger.error(e);
+                } finally {
+                    infinityForm.getPushTotalProgressBar().setIndeterminate(false);
+                }
+                break;
+            }
+
+            long currentTimeMillis = System.currentTimeMillis();
+            long lastTimeMillis = currentTimeMillis - startTimeMillis;
+            long leftTimeMillis = (long) ((double) lastTimeMillis / (PushData.sendSuccessList.size() + PushData.sendFailList.size()) * (PushData.allUser.size() - PushData.sendSuccessList.size() - PushData.sendFailList.size()));
+
+            // 耗时
+            String formatBetweenLast = DateUtil.formatBetween(lastTimeMillis, BetweenFormater.Level.SECOND);
+            infinityForm.getPushLastTimeLabel().setText("".equals(formatBetweenLast) ? "0s" : formatBetweenLast);
+
+            // 预计剩余
+            String formatBetweenLeft = DateUtil.formatBetween(leftTimeMillis, BetweenFormater.Level.SECOND);
+            infinityForm.getPushLeftTimeLabel().setText("".equals(formatBetweenLeft) ? "0s" : formatBetweenLeft);
+
+            infinityForm.getJvmMemoryLabel().setText("JVM内存占用：" + FileUtil.readableFileSize(Runtime.getRuntime().totalMemory()) + "/" + FileUtil.readableFileSize(Runtime.getRuntime().maxMemory()));
+
+            ThreadUtil.safeSleep(100);
+        }
+    }
+
+}
---
+++ src/main/java/com/fangxuele/tool/push/ui/listener/InfinityListener.java
@@ -0,0 +1,328 @@
+package com.fangxuele.tool.push.ui.listener;
+
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.pattern.CronPattern;
+import cn.hutool.cron.pattern.CronPatternUtil;
+import cn.hutool.cron.task.Task;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import com.fangxuele.tool.push.App;
+import com.fangxuele.tool.push.logic.BoostPushRunThread;
+import com.fangxuele.tool.push.logic.MessageTypeEnum;
+import com.fangxuele.tool.push.logic.PushControl;
+import com.fangxuele.tool.push.logic.PushData;
+import com.fangxuele.tool.push.ui.form.BoostForm;
+import com.fangxuele.tool.push.ui.form.InfinityForm;
+import com.fangxuele.tool.push.ui.form.MainWindow;
+import com.fangxuele.tool.push.ui.form.MessageEditForm;
+import com.fangxuele.tool.push.ui.form.ScheduleForm;
+import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.http.HttpResponse;
+
+import javax.swing.*;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <pre>
+ * 性能模式监听器
+ * </pre>
+ *
+ * @author <a href="https://github.com/rememberber">RememBerBer</a>
+ * @since 2019/7/3.
+ */
+public class InfinityListener {
+
+    private static final Log logger = LogFactory.get();
+
+    private static ScheduledExecutorService serviceStartAt;
+
+    private static ScheduledExecutorService serviceStartPerDay;
+
+    private static ScheduledExecutorService serviceStartPerWeek;
+
+    public static void addListeners() {
+        InfinityForm infinityForm = InfinityForm.getInstance();
+
+        // 开始按钮事件
+        infinityForm.getPushStartButton().addActionListener((e) -> ThreadUtil.execute(() -> {
+            PushData.boostMode = true;
+            if (App.config.getMsgType() != MessageTypeEnum.MP_TEMPLATE_CODE) {
+                JOptionPane.showMessageDialog(MainWindow.getInstance().getMainPanel(), "性能模式目前仅支持微信模板消息，后续逐步增加对其他消息类型的支持！", "提示",
+                        JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+            if (PushControl.pushCheck()) {
+                int isPush = JOptionPane.showConfirmDialog(infinityForm.getInfinityPanel(),
+                        "确定开始推送吗？\n\n推送消息：" +
+                                MessageEditForm.getInstance().getMsgNameField().getText() +
+                                "\n推送人数：" + PushData.allUser.size() +
+                                "\n\n空跑模式：" +
+                                infinityForm.getDryRunCheckBox().isSelected() + "\n", "确认推送？",
+                        JOptionPane.YES_NO_OPTION);
+                if (isPush == JOptionPane.YES_OPTION) {
+                    ThreadUtil.execute(new BoostPushRunThread());
+                }
+            }
+        }));
+
+        // 按计划执行按钮事件
+        infinityForm.getScheduleRunButton().addActionListener((e -> ThreadUtil.execute(() -> {
+            PushData.boostMode = true;
+            if (App.config.getMsgType() != MessageTypeEnum.MP_TEMPLATE_CODE) {
+                JOptionPane.showMessageDialog(MainWindow.getInstance().getMainPanel(), "性能模式目前仅支持微信模板消息，后续逐步增加对其他消息类型的支持！", "提示",
+                        JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+            if (PushControl.pushCheck()) {
+
+                // 看是否存在设置的计划任务
+                boolean existScheduleTask = false;
+
+                // 定时开始
+                if (App.config.isRadioStartAt()) {
+                    long startAtMills = DateUtil.parse(App.config.getTextStartAt(), DatePattern.NORM_DATETIME_PATTERN).getTime();
+                    if (startAtMills < System.currentTimeMillis()) {
+                        JOptionPane.showMessageDialog(infinityForm.getInfinityPanel(), "计划开始推送时间不能小于系统当前时间！\n\n请检查计划任务设置！\n\n", "提示",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        return;
+                    }
+
+                    int isSchedulePush = JOptionPane.showConfirmDialog(infinityForm.getInfinityPanel(),
+                            "将在" +
+                                    App.config.getTextStartAt() +
+                                    "推送\n\n消息：" +
+                                    MessageEditForm.getInstance().getMsgNameField().getText() +
+                                    "\n\n推送人数：" + PushData.allUser.size() +
+                                    "\n\n空跑模式：" +
+                                    infinityForm.getDryRunCheckBox().isSelected(), "确认定时推送？",
+                            JOptionPane.YES_NO_OPTION);
+                    if (isSchedulePush == JOptionPane.YES_OPTION) {
+                        PushData.scheduling = true;
+                        // 按钮状态
+                        infinityForm.getScheduleRunButton().setEnabled(false);
+                        infinityForm.getPushStartButton().setEnabled(false);
+                        infinityForm.getPushStopButton().setText("停止计划任务");
+                        infinityForm.getPushStopButton().setEnabled(true);
+
+                        infinityForm.getScheduleDetailLabel().setVisible(true);
+                        infinityForm.getScheduleDetailLabel().setText("计划任务执行中：将在" +
+                                App.config.getTextStartAt() +
+                                "开始推送");
+
+                        serviceStartAt = Executors.newSingleThreadScheduledExecutor();
+                        serviceStartAt.schedule(new BoostPushRunThread(), startAtMills - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+                    }
+                    existScheduleTask = true;
+                }
+
+                // 每天固定时间开始
+                if (App.config.isRadioPerDay()) {
+                    long startPerDayMills = DateUtil.parse(DateUtil.today() + " " + App.config.getTextPerDay(), DatePattern.NORM_DATETIME_PATTERN).getTime();
+
+                    int isSchedulePush = JOptionPane.showConfirmDialog(infinityForm.getInfinityPanel(),
+                            "将在每天" +
+                                    App.config.getTextPerDay() +
+                                    "推送\n\n消息：" +
+                                    MessageEditForm.getInstance().getMsgNameField().getText() +
+                                    "\n\n推送人数：" + PushData.allUser.size() +
+                                    "\n\n空跑模式：" +
+                                    infinityForm.getDryRunCheckBox().isSelected(), "确认定时推送？",
+                            JOptionPane.YES_NO_OPTION);
+                    if (isSchedulePush == JOptionPane.YES_OPTION) {
+                        PushData.fixRateScheduling = true;
+                        // 按钮状态
+                        infinityForm.getScheduleRunButton().setEnabled(false);
+                        infinityForm.getPushStartButton().setEnabled(false);
+                        infinityForm.getPushStopButton().setText("停止计划任务");
+                        infinityForm.getPushStopButton().setEnabled(true);
+
+                        infinityForm.getScheduleDetailLabel().setVisible(true);
+                        infinityForm.getScheduleDetailLabel().setText("计划任务执行中：将在每天" +
+                                App.config.getTextPerDay() +
+                                "开始推送");
+
+                        serviceStartPerDay = Executors.newSingleThreadScheduledExecutor();
+                        long millisBetween = startPerDayMills - System.currentTimeMillis();
+                        long delay = millisBetween < 0 ? millisBetween + 24 * 60 * 60 * 1000 : millisBetween;
+                        serviceStartPerDay.scheduleAtFixedRate(new BoostPushRunThread(), delay, 24 * 60 * 60 * 1000, TimeUnit.MILLISECONDS);
+                    }
+                    existScheduleTask = true;
+                }
+
+                // 每周固定时间开始
+                if (App.config.isRadioPerWeek()) {
+
+                    long todaySetMills = DateUtil.parse(DateUtil.today() + " " + App.config.getTextPerWeekTime(), DatePattern.NORM_DATETIME_PATTERN).getTime();
+                    int dayBetween = ScheduleForm.getDayOfWeek(App.config.getTextPerWeekWeek()) - DateUtil.thisDayOfWeek();
+                    long startPerWeekMills = dayBetween < 0 ? (dayBetween + 7) * 24 * 60 * 60 * 1000 : dayBetween * 24 * 60 * 60 * 1000;
+
+                    int isSchedulePush = JOptionPane.showConfirmDialog(infinityForm.getInfinityPanel(),
+                            "将在每周" + App.config.getTextPerWeekWeek() +
+                                    App.config.getTextPerWeekTime() +
+                                    "推送\n\n消息：" +
+                                    MessageEditForm.getInstance().getMsgNameField().getText() +
+                                    "\n\n推送人数：" + PushData.allUser.size() +
+                                    "\n\n空跑模式：" +
+                                    infinityForm.getDryRunCheckBox().isSelected(), "确认定时推送？",
+                            JOptionPane.YES_NO_OPTION);
+                    if (isSchedulePush == JOptionPane.YES_OPTION) {
+                        PushData.scheduling = true;
+                        PushData.fixRateScheduling = true;
+                        // 按钮状态
+                        infinityForm.getScheduleRunButton().setEnabled(false);
+                        infinityForm.getPushStartButton().setEnabled(false);
+                        infinityForm.getPushStopButton().setText("停止计划任务");
+                        infinityForm.getPushStopButton().setEnabled(true);
+
+                        infinityForm.getScheduleDetailLabel().setVisible(true);
+                        infinityForm.getScheduleDetailLabel().setText("计划任务执行中：将在每周" +
+                                App.config.getTextPerWeekWeek() +
+                                App.config.getTextPerWeekTime() +
+                                "开始推送");
+
+                        serviceStartPerWeek = Executors.newSingleThreadScheduledExecutor();
+                        long millisBetween = startPerWeekMills + todaySetMills - System.currentTimeMillis();
+                        long delay = millisBetween < 0 ? millisBetween + 7 * 24 * 60 * 60 * 1000 : millisBetween;
+                        serviceStartPerWeek.scheduleAtFixedRate(new BoostPushRunThread(), delay, 7 * 24 * 60 * 60 * 1000, TimeUnit.MILLISECONDS);
+                    }
+                    existScheduleTask = true;
+                }
+
+                // 按Cron表达式触发
+                if (App.config.isRadioCron()) {
+
+                    List<String> latest5RunTimeList = Lists.newArrayList();
+                    Date now = new Date();
+                    for (int i = 0; i < 5; i++) {
+                        Date date = CronPatternUtil.nextDateAfter(new CronPattern(App.config.getTextCron()), DateUtils.addDays(now, i), true);
+                        latest5RunTimeList.add(DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss"));
+                    }
+
+                    int isSchedulePush = JOptionPane.showConfirmDialog(infinityForm.getInfinityPanel(),
+                            "将按" +
+                                    App.config.getTextCron() +
+                                    "表达式触发推送\n\n" +
+                                    "最近5次运行时间:\n" +
+                                    String.join("\n", latest5RunTimeList) +
+                                    "\n\n消息名称：" +
+                                    MessageEditForm.getInstance().getMsgNameField().getText() +
+                                    "\n推送人数：" + PushData.allUser.size() +
+                                    "\n空跑模式：" +
+                                    infinityForm.getDryRunCheckBox().isSelected(), "确认定时推送？",
+                            JOptionPane.YES_NO_OPTION);
+                    if (isSchedulePush == JOptionPane.YES_OPTION) {
+                        PushData.fixRateScheduling = true;
+                        // 按钮状态
+                        infinityForm.getScheduleRunButton().setEnabled(false);
+                        infinityForm.getPushStartButton().setEnabled(false);
+                        infinityForm.getPushStopButton().setText("停止计划任务");
+                        infinityForm.getPushStopButton().setEnabled(true);
+
+                        infinityForm.getScheduleDetailLabel().setVisible(true);
+                        infinityForm.getScheduleDetailLabel().setText("计划任务执行中，下一次执行时间：" + latest5RunTimeList.get(0));
+
+                        // 支持秒级别定时任务
+                        CronUtil.setMatchSecond(true);
+                        CronUtil.schedule(App.config.getTextCron(), (Task) () -> new BoostPushRunThread().start());
+                        CronUtil.start();
+                    }
+                    existScheduleTask = true;
+                }
+
+                if (!existScheduleTask) {
+                    JOptionPane.showMessageDialog(infinityForm.getInfinityPanel(), "请先设置计划任务！", "提示",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        })));
+
+        // 停止按钮事件
+        infinityForm.getPushStopButton().addActionListener((e) -> {
+            ThreadUtil.execute(() -> {
+                if (PushData.scheduling) {
+                    infinityForm.getScheduleDetailLabel().setText("");
+                    if (serviceStartAt != null) {
+                        serviceStartAt.shutdownNow();
+                    }
+                    infinityForm.getPushStartButton().setEnabled(true);
+                    infinityForm.getScheduleRunButton().setEnabled(true);
+                    infinityForm.getPushStopButton().setText("停止");
+                    infinityForm.getPushStopButton().setEnabled(false);
+                    infinityForm.getPushStartButton().updateUI();
+                    infinityForm.getScheduleRunButton().updateUI();
+                    infinityForm.getPushStopButton().updateUI();
+                    infinityForm.getScheduleDetailLabel().setVisible(false);
+                    PushData.scheduling = false;
+                    PushData.running = false;
+                }
+
+                if (PushData.fixRateScheduling) {
+                    infinityForm.getScheduleDetailLabel().setText("");
+                    if (serviceStartPerDay != null) {
+                        serviceStartPerDay.shutdownNow();
+                    }
+                    if (serviceStartPerWeek != null) {
+                        serviceStartPerWeek.shutdownNow();
+                    }
+                    try {
+                        CronUtil.stop();
+                    } catch (Exception e1) {
+                        logger.warn(e1.toString());
+                    }
+                    infinityForm.getPushStartButton().setEnabled(true);
+                    infinityForm.getScheduleRunButton().setEnabled(true);
+                    infinityForm.getPushStopButton().setText("停止");
+                    infinityForm.getPushStopButton().setEnabled(false);
+                    infinityForm.getPushStartButton().updateUI();
+                    infinityForm.getScheduleRunButton().updateUI();
+                    infinityForm.getPushStopButton().updateUI();
+                    infinityForm.getScheduleDetailLabel().setVisible(false);
+                    PushData.fixRateScheduling = false;
+                    PushData.running = false;
+                }
+
+                if (PushData.running) {
+                    int isStop = JOptionPane.showConfirmDialog(infinityForm.getInfinityPanel(),
+                            "确定停止当前的推送吗？", "确认停止？",
+                            JOptionPane.YES_NO_OPTION);
+                    if (isStop == JOptionPane.YES_OPTION) {
+                        PushData.running = false;
+                        infinityForm.getPushStartButton().setEnabled(true);
+                        infinityForm.getScheduleRunButton().setEnabled(true);
+                        infinityForm.getPushStopButton().setText("停止");
+                        infinityForm.getPushStopButton().setEnabled(false);
+                        infinityForm.getPushStartButton().updateUI();
+                        infinityForm.getScheduleRunButton().updateUI();
+                        infinityForm.getPushStopButton().updateUI();
+                        infinityForm.getScheduleDetailLabel().setVisible(false);
+                    }
+                }
+                for (Future<HttpResponse> httpResponseFuture : BoostPushRunThread.futureList) {
+                    httpResponseFuture.cancel(true);
+                }
+            });
+        });
+    }
+
+    static void refreshPushInfo() {
+        BoostForm boostForm = BoostForm.getInstance();
+        // 总记录数
+        long totalCount = PushData.allUser.size();
+        boostForm.getMemberCountLabel().setText("消息总数：" + totalCount);
+        // 可用处理器核心
+        boostForm.getProcessorCountLabel().setText("可用处理器核心：" + Runtime.getRuntime().availableProcessors());
+        // JVM内存占用
+        boostForm.getJvmMemoryLabel().setText("JVM内存占用：" + FileUtil.readableFileSize(Runtime.getRuntime().totalMemory()) + "/" + FileUtil.readableFileSize(Runtime.getRuntime().maxMemory()));
+    }
+}
