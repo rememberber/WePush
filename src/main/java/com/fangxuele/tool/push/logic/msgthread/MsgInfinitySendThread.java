8ea974a52fe9184532a2550a64decd37a0368982
zhoubo58 <zhoubo9@jd.com>
2021-01-20 12:37:58 +0800

    fix:变速模式无法保存推送结果

--- src/main/java/com/fangxuele/tool/push/logic/msgthread/MsgInfinitySendThread.java
+++ src/main/java/com/fangxuele/tool/push/logic/msgthread/MsgInfinitySendThread.java
@@ -30,24 +30,30 @@ public class MsgInfinitySendThread extends Thread {
     public void run() {
 
         while (PushData.running && PushData.threadStatusMap.get(this.getName()) && !PushData.toSendConcurrentLinkedQueue.isEmpty()) {
+            String[] msgData = PushData.toSendConcurrentLinkedQueue.poll();
+            if (msgData == null) {
+                continue;
+            }
             try {
-                String[] msgData = PushData.toSendConcurrentLinkedQueue.poll();
-                if (msgData == null) {
-                    continue;
-                }
                 SendResult sendResult = iMsgSender.send(msgData);
                 if (sendResult.isSuccess()) {
                     PushData.increaseSuccess();
+                    // 保存发送成功
 //                    ConsoleUtil.infinityConsoleOnly(Thread.currentThread().getName() + "：发送成功：" + msgData[0]);
+                    PushData.sendSuccessList.add(msgData);
                 } else {
                     PushData.increaseFail();
                     InfinityForm.getInstance().getPushFailCount().setText(String.valueOf(PushData.failRecords));
+                    // 保存发送失败
+                    PushData.sendFailList.add(msgData);
                     ConsoleUtil.infinityConsoleWithLog("发送失败:" + sendResult.getInfo() + ";msgData:" + JSONUtil.toJsonPrettyStr(msgData));
                 }
             } catch (Exception e) {
                 PushData.increaseFail();
                 InfinityForm.getInstance().getPushFailCount().setText(String.valueOf(PushData.failRecords));
                 ConsoleUtil.infinityConsoleWithLog("发送异常：" + ExceptionUtils.getStackTrace(e));
+                // 保存发送失败
+                PushData.sendFailList.add(msgData);
             }
             // 已处理+1
             PushData.increaseProcessed();
