52fffe4ec435d9e48b1dd27aff1a1a0f519d371b
zhoubo58 <zhoubo9@jd.com>
2021-04-28 14:07:14 +0800

    人群编辑-通过钉钉通讯录导入-初步

--- src/main/java/com/fangxuele/tool/push/ui/listener/PeopleEditListener.java
+++ src/main/java/com/fangxuele/tool/push/ui/listener/PeopleEditListener.java
@@ -3,10 +3,7 @@ package com.fangxuele.tool.push.ui.listener;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import com.fangxuele.tool.push.dao.TPeopleMapper;
-import com.fangxuele.tool.push.ui.dialog.importway.ImportByFile;
-import com.fangxuele.tool.push.ui.dialog.importway.ImportBySQL;
-import com.fangxuele.tool.push.ui.dialog.importway.ImportByWxCp;
-import com.fangxuele.tool.push.ui.dialog.importway.ImportByWxMp;
+import com.fangxuele.tool.push.ui.dialog.importway.*;
 import com.fangxuele.tool.push.ui.form.MainWindow;
 import com.fangxuele.tool.push.ui.form.PeopleEditForm;
 import com.fangxuele.tool.push.util.MybatisUtil;
@@ -34,6 +31,7 @@ public class PeopleEditListener {
         // 导入按钮
         peopleEditForm.getImportButton().addActionListener(e -> {
             JPopupMenu popupMenu = new JPopupMenu();
+
             JMenuItem menuItem1 = new JMenuItem();
             menuItem1.setText("通过文件导入");
             menuItem1.addActionListener(e1 -> {
@@ -41,6 +39,7 @@ public class PeopleEditListener {
                 showImportDialog(actionCommand);
             });
             popupMenu.add(menuItem1);
+
             JMenuItem menuItem2 = new JMenuItem();
             menuItem2.setText("通过SQL导入");
             menuItem2.addActionListener(e1 -> {
@@ -48,6 +47,7 @@ public class PeopleEditListener {
                 showImportDialog(actionCommand);
             });
             popupMenu.add(menuItem2);
+
             JMenuItem menuItem3 = new JMenuItem();
             menuItem3.setText("通过微信公众平台导入");
             menuItem3.addActionListener(e1 -> {
@@ -55,6 +55,7 @@ public class PeopleEditListener {
                 showImportDialog(actionCommand);
             });
             popupMenu.add(menuItem3);
+
             JMenuItem menuItem4 = new JMenuItem();
             menuItem4.setText("通过微信企业通讯录导入");
             menuItem4.addActionListener(e1 -> {
@@ -62,8 +63,17 @@ public class PeopleEditListener {
                 showImportDialog(actionCommand);
             });
             popupMenu.add(menuItem4);
+
+            JMenuItem menuItem5 = new JMenuItem();
+            menuItem5.setText("通过钉钉通讯录导入");
+            menuItem5.addActionListener(e1 -> {
+                String actionCommand = e1.getActionCommand();
+                showImportDialog(actionCommand);
+            });
+            popupMenu.add(menuItem5);
+
             peopleEditForm.getImportButton().setComponentPopupMenu(popupMenu);
-            peopleEditForm.getImportButton().getComponentPopupMenu().show(peopleEditForm.getImportButton(), -peopleEditForm.getImportButton().getWidth(), -peopleEditForm.getImportButton().getHeight() * 4);
+            peopleEditForm.getImportButton().getComponentPopupMenu().show(peopleEditForm.getImportButton(), -peopleEditForm.getImportButton().getWidth(), -peopleEditForm.getImportButton().getHeight() * 5);
         });
     }
 
@@ -82,6 +92,10 @@ public class PeopleEditListener {
             dialog.setVisible(true);
         } else if ("通过微信企业通讯录导入".equals(actionCommand)) {
             ImportByWxCp dialog = new ImportByWxCp();
+            dialog.pack();
+            dialog.setVisible(true);
+        } else if ("通过钉钉通讯录导入".equals(actionCommand)) {
+            ImportByDing dialog = new ImportByDing();
             dialog.pack();
             dialog.setVisible(true);
         } else {
