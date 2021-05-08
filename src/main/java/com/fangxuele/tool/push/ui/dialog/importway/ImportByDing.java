60d4100fd9af78ab3ae556569f2c8b26cc53389e
zhoubo58 <zhoubo9@jd.com>
2021-05-08 16:39:08 +0800

    通过钉钉导入功能实现完毕

--- src/main/java/com/fangxuele/tool/push/ui/dialog/importway/ImportByDing.java
+++ src/main/java/com/fangxuele/tool/push/ui/dialog/importway/ImportByDing.java
@@ -1,7 +1,33 @@
 package com.fangxuele.tool.push.ui.dialog.importway;
 
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiDepartmentListRequest;
+import com.dingtalk.api.request.OapiUserSimplelistRequest;
+import com.dingtalk.api.response.OapiDepartmentListResponse;
+import com.dingtalk.api.response.OapiUserSimplelistResponse;
 import com.fangxuele.tool.push.App;
+import com.fangxuele.tool.push.dao.TPeopleDataMapper;
+import com.fangxuele.tool.push.dao.TPeopleImportConfigMapper;
+import com.fangxuele.tool.push.domain.TPeopleData;
+import com.fangxuele.tool.push.domain.TPeopleImportConfig;
+import com.fangxuele.tool.push.logic.PeopleImportWayEnum;
+import com.fangxuele.tool.push.logic.PushData;
+import com.fangxuele.tool.push.logic.msgsender.DingMsgSender;
+import com.fangxuele.tool.push.ui.UiConsts;
+import com.fangxuele.tool.push.ui.form.MainWindow;
+import com.fangxuele.tool.push.ui.form.MemberForm;
+import com.fangxuele.tool.push.ui.form.PeopleEditForm;
+import com.fangxuele.tool.push.ui.form.msg.DingMsgForm;
+import com.fangxuele.tool.push.ui.listener.PeopleManageListener;
 import com.fangxuele.tool.push.util.ComponentUtil;
+import com.fangxuele.tool.push.util.MybatisUtil;
+import com.fangxuele.tool.push.util.SqliteUtil;
+import com.google.common.collect.Maps;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 
@@ -10,7 +36,9 @@ import javax.swing.plaf.FontUIResource;
 import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class ImportByDing extends JDialog {
     private JPanel contentPane;
@@ -19,12 +47,166 @@ public class ImportByDing extends JDialog {
     private JButton dingDeptsRefreshButton;
     private JButton dingDeptsImportButton;
 
+    private static final Log logger = LogFactory.get();
+
+    private static TPeopleDataMapper peopleDataMapper = MybatisUtil.getSqlSession().getMapper(TPeopleDataMapper.class);
+    private static TPeopleImportConfigMapper peopleImportConfigMapper = MybatisUtil.getSqlSession().getMapper(TPeopleImportConfigMapper.class);
+
+    /**
+     * 企业号部门名称->部门ID
+     */
+    private static Map<String, Long> wxCpDeptNameToIdMap = Maps.newHashMap();
+
+    /**
+     * 企业号部门ID>部门名称
+     */
+    private static Map<Long, String> wxCpIdToDeptNameMap = Maps.newHashMap();
+
     public ImportByDing() {
         super(App.mainFrame, "通过钉钉通讯录导入");
         setContentPane(contentPane);
         setModal(true);
         ComponentUtil.setPreferSizeAndLocateToCenter(this, 0.3, 0.2);
         getRootPane().setDefaultButton(dingImportAllButton);
+
+        // 钉钉-按部门导入-刷新
+        dingDeptsRefreshButton.addActionListener(e -> {
+            ThreadUtil.execute(() -> {
+                if (DingMsgForm.getInstance().getAppNameComboBox().getSelectedItem() == null) {
+                    JOptionPane.showMessageDialog(MainWindow.getInstance().getMessagePanel(), "请先在编辑消息tab中选择应用！", "提示",
+                            JOptionPane.ERROR_MESSAGE);
+                    MainWindow.getInstance().getTabbedPane().setSelectedIndex(2);
+                    return;
+                }
+                dingDeptsComboBox.removeAllItems();
+
+                try {
+                    // 获取部门列表
+                    DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/department/list");
+                    OapiDepartmentListRequest request = new OapiDepartmentListRequest();
+                    request.setHttpMethod("GET");
+                    OapiDepartmentListResponse response = client.execute(request, DingMsgSender.getAccessTokenTimedCache().get("accessToken"));
+                    if (response.getErrcode() != 0) {
+                        JOptionPane.showMessageDialog(App.mainFrame, "刷新失败！\n\n" + response.getErrmsg(), "失败",
+                                JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+                    List<OapiDepartmentListResponse.Department> departmentList = response.getDepartment();
+                    for (OapiDepartmentListResponse.Department department : departmentList) {
+                        dingDeptsComboBox.addItem(department.getName());
+                        wxCpDeptNameToIdMap.put(department.getName(), department.getId());
+                        wxCpIdToDeptNameMap.put(department.getId(), department.getName());
+                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(App.mainFrame, "刷新失败！\n\n" + ex, "失败",
+                            JOptionPane.ERROR_MESSAGE);
+                    logger.error(ex.toString());
+                }
+            });
+        });
+
+        // 钉钉-按部门导入-导入
+        dingDeptsImportButton.addActionListener(e -> {
+            ThreadUtil.execute(() -> {
+                PeopleEditForm peopleEditForm = PeopleEditForm.getInstance();
+                JProgressBar progressBar = peopleEditForm.getMemberTabImportProgressBar();
+                JLabel memberCountLabel = peopleEditForm.getMemberTabCountLabel();
+
+                if (dingDeptsComboBox.getSelectedItem() == null) {
+                    return;
+                }
+                try {
+                    progressBar.setVisible(true);
+                    progressBar.setIndeterminate(true);
+                    int importedCount = 0;
+                    String now = SqliteUtil.nowDateForSqlite();
+
+                    // 保存导入配置
+                    TPeopleImportConfig beforePeopleImportConfig = peopleImportConfigMapper.selectByPeopleId(PeopleManageListener.selectedPeopleId);
+
+                    TPeopleImportConfig tPeopleImportConfig = new TPeopleImportConfig();
+                    tPeopleImportConfig.setPeopleId(PeopleManageListener.selectedPeopleId);
+                    tPeopleImportConfig.setLastWay(String.valueOf(PeopleImportWayEnum.BY_DING_CODE));
+                    tPeopleImportConfig.setAppVersion(UiConsts.APP_VERSION);
+                    tPeopleImportConfig.setModifiedTime(now);
+
+                    if (beforePeopleImportConfig != null) {
+                        tPeopleImportConfig.setId(beforePeopleImportConfig.getId());
+                        peopleImportConfigMapper.updateByPrimaryKeySelective(tPeopleImportConfig);
+                    } else {
+                        tPeopleImportConfig.setCreateTime(now);
+                        peopleImportConfigMapper.insert(tPeopleImportConfig);
+                    }
+
+                    // 获取部门id
+                    Long deptId = wxCpDeptNameToIdMap.get(dingDeptsComboBox.getSelectedItem());
+                    // 获取用户
+                    DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/user/simplelist");
+                    OapiUserSimplelistRequest request = new OapiUserSimplelistRequest();
+                    request.setDepartmentId(deptId);
+                    request.setOffset(0L);
+                    request.setSize(100L);
+                    request.setHttpMethod("GET");
+
+                    long offset = 0;
+                    OapiUserSimplelistResponse response = new OapiUserSimplelistResponse();
+                    while (response.getErrcode() == null || response.getUserlist().size() > 0) {
+                        response = client.execute(request, DingMsgSender.getAccessTokenTimedCache().get("accessToken"));
+                        if (response.getErrcode() != 0) {
+                            if (response.getErrcode() == 60011) {
+                                JOptionPane.showMessageDialog(App.mainFrame, "导入失败！\n\n" + response.getErrmsg() + "\n\n进入开发者后台，在小程序或者微应用详情的「接口权限」模块，点击申请对应的通讯录接口读写权限", "失败",
+                                        JOptionPane.ERROR_MESSAGE);
+                            } else {
+                                JOptionPane.showMessageDialog(App.mainFrame, "导入失败！\n\n" + response.getErrmsg(), "失败", JOptionPane.ERROR_MESSAGE);
+                            }
+
+                            logger.error(response.getErrmsg());
+                            return;
+                        }
+                        List<OapiUserSimplelistResponse.Userlist> userlist = response.getUserlist();
+                        for (OapiUserSimplelistResponse.Userlist dingUser : userlist) {
+                            String[] dataArray = new String[]{dingUser.getUserid(), dingUser.getName()};
+
+                            TPeopleData tPeopleData = new TPeopleData();
+                            tPeopleData.setPeopleId(PeopleManageListener.selectedPeopleId);
+                            tPeopleData.setPin(dataArray[0]);
+                            tPeopleData.setVarData(JSONUtil.toJsonStr(dataArray));
+                            tPeopleData.setAppVersion(UiConsts.APP_VERSION);
+                            tPeopleData.setCreateTime(now);
+                            tPeopleData.setModifiedTime(now);
+
+                            peopleDataMapper.insert(tPeopleData);
+
+                            importedCount++;
+                            memberCountLabel.setText(String.valueOf(importedCount));
+                        }
+                        offset += 100;
+                        request.setOffset(offset);
+                    }
+
+                    PeopleEditForm.initDataTable(PeopleManageListener.selectedPeopleId);
+
+                    if (!PushData.fixRateScheduling) {
+                        JOptionPane.showMessageDialog(App.mainFrame, "导入完成！", "完成", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(App.mainFrame, "导入失败！\n\n" + ex, "失败",
+                            JOptionPane.ERROR_MESSAGE);
+                    logger.error(ex.toString());
+                } finally {
+                    progressBar.setIndeterminate(false);
+                    progressBar.setVisible(false);
+                }
+
+            });
+        });
+
+        // 钉钉-导入全部
+        dingImportAllButton.addActionListener(e -> {
+            ThreadUtil.execute(() -> {
+                importDingAll();
+            });
+        });
 
         dingImportAllButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -56,6 +238,104 @@ public class ImportByDing extends JDialog {
     private void onCancel() {
         // add your code here if necessary
         dispose();
+    }
+
+
+    /**
+     * 导入钉钉通讯录全员
+     */
+    public static void importDingAll() {
+        JProgressBar progressBar = MemberForm.getInstance().getMemberTabImportProgressBar();
+        JLabel memberCountLabel = MemberForm.getInstance().getMemberTabCountLabel();
+
+        try {
+            if (DingMsgForm.getInstance().getAppNameComboBox().getSelectedItem() == null) {
+                JOptionPane.showMessageDialog(MainWindow.getInstance().getMessagePanel(), "请先在编辑消息tab中选择应用！", "提示",
+                        JOptionPane.ERROR_MESSAGE);
+                MainWindow.getInstance().getTabbedPane().setSelectedIndex(2);
+                return;
+            }
+
+            progressBar.setVisible(true);
+            progressBar.setIndeterminate(true);
+            int importedCount = 0;
+            String now = SqliteUtil.nowDateForSqlite();
+
+            // 保存导入配置
+            TPeopleImportConfig beforePeopleImportConfig = peopleImportConfigMapper.selectByPeopleId(PeopleManageListener.selectedPeopleId);
+
+            TPeopleImportConfig tPeopleImportConfig = new TPeopleImportConfig();
+            tPeopleImportConfig.setPeopleId(PeopleManageListener.selectedPeopleId);
+            tPeopleImportConfig.setLastWay(String.valueOf(PeopleImportWayEnum.BY_DING_CODE));
+            tPeopleImportConfig.setAppVersion(UiConsts.APP_VERSION);
+            tPeopleImportConfig.setModifiedTime(now);
+
+            if (beforePeopleImportConfig != null) {
+                tPeopleImportConfig.setId(beforePeopleImportConfig.getId());
+                peopleImportConfigMapper.updateByPrimaryKeySelective(tPeopleImportConfig);
+            } else {
+                tPeopleImportConfig.setCreateTime(now);
+                peopleImportConfigMapper.insert(tPeopleImportConfig);
+            }
+
+            // 最小部门id为1
+            // 获取用户
+            DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/user/simplelist");
+            OapiUserSimplelistRequest request = new OapiUserSimplelistRequest();
+            request.setDepartmentId(1L);
+            request.setOffset(0L);
+            request.setSize(100L);
+            request.setHttpMethod("GET");
+
+            long offset = 0;
+            OapiUserSimplelistResponse response = new OapiUserSimplelistResponse();
+            while (response.getErrcode() == null || response.getUserlist().size() > 0) {
+                response = client.execute(request, DingMsgSender.getAccessTokenTimedCache().get("accessToken"));
+                if (response.getErrcode() != 0) {
+                    if (response.getErrcode() == 60011) {
+                        JOptionPane.showMessageDialog(App.mainFrame, "导入失败！\n\n" + response.getErrmsg() + "\n\n进入开发者后台，在小程序或者微应用详情的「接口权限」模块，点击申请对应的通讯录接口读写权限", "失败",
+                                JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(App.mainFrame, "导入失败！\n\n" + response.getErrmsg(), "失败", JOptionPane.ERROR_MESSAGE);
+                    }
+
+                    logger.error(response.getErrmsg());
+                    return;
+                }
+                List<OapiUserSimplelistResponse.Userlist> userlist = response.getUserlist();
+                for (OapiUserSimplelistResponse.Userlist dingUser : userlist) {
+                    String[] dataArray = new String[]{dingUser.getUserid(), dingUser.getName()};
+
+                    TPeopleData tPeopleData = new TPeopleData();
+                    tPeopleData.setPeopleId(PeopleManageListener.selectedPeopleId);
+                    tPeopleData.setPin(dataArray[0]);
+                    tPeopleData.setVarData(JSONUtil.toJsonStr(dataArray));
+                    tPeopleData.setAppVersion(UiConsts.APP_VERSION);
+                    tPeopleData.setCreateTime(now);
+                    tPeopleData.setModifiedTime(now);
+
+                    peopleDataMapper.insert(tPeopleData);
+
+                    importedCount++;
+                    memberCountLabel.setText(String.valueOf(importedCount));
+                }
+                offset += 100;
+                request.setOffset(offset);
+            }
+
+            PeopleEditForm.initDataTable(PeopleManageListener.selectedPeopleId);
+
+            if (!PushData.fixRateScheduling) {
+                JOptionPane.showMessageDialog(App.mainFrame, "导入完成！", "完成", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(App.mainFrame, "导入失败！\n\n" + ex, "失败",
+                    JOptionPane.ERROR_MESSAGE);
+            logger.error(ex.toString());
+        } finally {
+            progressBar.setIndeterminate(false);
+            progressBar.setVisible(false);
+        }
     }
 
     {
