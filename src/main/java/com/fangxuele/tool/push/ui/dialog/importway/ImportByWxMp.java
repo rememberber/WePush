c73040caaff64f43206ab1dc9d59282fb84ec562
zhoubo58 <zhoubo9@jd.com>
2021-05-10 14:14:07 +0800

    通过公众平台导入功能初步实现完毕-有bug

--- src/main/java/com/fangxuele/tool/push/ui/dialog/importway/ImportByWxMp.java
+++ src/main/java/com/fangxuele/tool/push/ui/dialog/importway/ImportByWxMp.java
@@ -1,41 +1,38 @@
 package com.fangxuele.tool.push.ui.dialog.importway;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.json.JSONUtil;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import com.fangxuele.tool.push.App;
 import com.fangxuele.tool.push.dao.TPeopleDataMapper;
 import com.fangxuele.tool.push.dao.TPeopleImportConfigMapper;
 import com.fangxuele.tool.push.dao.TWxMpUserMapper;
-import com.fangxuele.tool.push.domain.TWxMpUser;
-import com.fangxuele.tool.push.logic.MessageTypeEnum;
+import com.fangxuele.tool.push.domain.TPeopleData;
+import com.fangxuele.tool.push.domain.TPeopleImportConfig;
+import com.fangxuele.tool.push.logic.PeopleImportWayEnum;
 import com.fangxuele.tool.push.logic.PushData;
 import com.fangxuele.tool.push.logic.msgsender.WxMpTemplateMsgSender;
-import com.fangxuele.tool.push.ui.component.TableInCellImageLabelRenderer;
+import com.fangxuele.tool.push.ui.UiConsts;
 import com.fangxuele.tool.push.ui.form.MemberForm;
 import com.fangxuele.tool.push.ui.form.PeopleEditForm;
+import com.fangxuele.tool.push.ui.listener.PeopleManageListener;
 import com.fangxuele.tool.push.util.ComponentUtil;
 import com.fangxuele.tool.push.util.ConsoleUtil;
-import com.fangxuele.tool.push.util.JTableUtil;
 import com.fangxuele.tool.push.util.MybatisUtil;
+import com.fangxuele.tool.push.util.SqliteUtil;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import me.chanjar.weixin.mp.bean.result.WxMpUserList;
 import me.chanjar.weixin.mp.bean.tag.WxTagListUser;
 import me.chanjar.weixin.mp.bean.tag.WxUserTag;
-import org.apache.commons.compress.utils.Lists;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
 
 import javax.swing.*;
 import javax.swing.plaf.FontUIResource;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.awt.event.*;
@@ -117,7 +114,7 @@ public class ImportByWxMp extends JDialog {
 
                     long selectedTagId = userTagMap.get(memberImportTagComboBox.getSelectedItem());
                     getMpUserListByTag(selectedTagId, false);
-                    renderMemberListTable();
+                    PeopleEditForm.initDataTable(PeopleManageListener.selectedPeopleId);
                     JOptionPane.showMessageDialog(App.mainFrame, "导入完成！", "完成",
                             JOptionPane.INFORMATION_MESSAGE);
                 } else {
@@ -147,7 +144,7 @@ public class ImportByWxMp extends JDialog {
 
                     long selectedTagId = userTagMap.get(memberImportTagComboBox.getSelectedItem());
                     getMpUserListByTag(selectedTagId, true);
-                    renderMemberListTable();
+                    PeopleEditForm.initDataTable(PeopleManageListener.selectedPeopleId);
                     JOptionPane.showMessageDialog(App.mainFrame, "导入完成！", "完成",
                             JOptionPane.INFORMATION_MESSAGE);
                 } else {
@@ -209,7 +206,7 @@ public class ImportByWxMp extends JDialog {
 
         try {
             getMpUserList();
-            renderMemberListTable();
+            PeopleEditForm.initDataTable(PeopleManageListener.selectedPeopleId);
             if (!PushData.fixRateScheduling) {
                 JOptionPane.showMessageDialog(App.mainFrame, "导入完成！", "完成", JOptionPane.INFORMATION_MESSAGE);
             }
@@ -248,7 +245,24 @@ public class ImportByWxMp extends JDialog {
         progressBar.setIndeterminate(false);
         progressBar.setMaximum((int) wxMpUserList.getTotal());
         int importedCount = 0;
-        PushData.allUser = Collections.synchronizedList(new ArrayList<>());
+        String now = SqliteUtil.nowDateForSqlite();
+
+        // 保存导入配置
+        TPeopleImportConfig beforePeopleImportConfig = peopleImportConfigMapper.selectByPeopleId(PeopleManageListener.selectedPeopleId);
+
+        TPeopleImportConfig tPeopleImportConfig = new TPeopleImportConfig();
+        tPeopleImportConfig.setPeopleId(PeopleManageListener.selectedPeopleId);
+        tPeopleImportConfig.setLastWay(String.valueOf(PeopleImportWayEnum.BY_NUM_CODE));
+        tPeopleImportConfig.setAppVersion(UiConsts.APP_VERSION);
+        tPeopleImportConfig.setModifiedTime(now);
+
+        if (beforePeopleImportConfig != null) {
+            tPeopleImportConfig.setId(beforePeopleImportConfig.getId());
+            peopleImportConfigMapper.updateByPrimaryKeySelective(tPeopleImportConfig);
+        } else {
+            tPeopleImportConfig.setCreateTime(now);
+            peopleImportConfigMapper.insert(tPeopleImportConfig);
+        }
 
         if (wxMpUserList.getCount() == 0) {
             memberCountLabel.setText(String.valueOf(importedCount));
@@ -259,7 +273,18 @@ public class ImportByWxMp extends JDialog {
         List<String> openIds = wxMpUserList.getOpenids();
 
         for (String openId : openIds) {
-            PushData.allUser.add(new String[]{openId});
+            String[] array = {openId};
+
+            TPeopleData tPeopleData = new TPeopleData();
+            tPeopleData.setPeopleId(PeopleManageListener.selectedPeopleId);
+            tPeopleData.setPin(array[0]);
+            tPeopleData.setVarData(JSONUtil.toJsonStr(array));
+            tPeopleData.setAppVersion(UiConsts.APP_VERSION);
+            tPeopleData.setCreateTime(now);
+            tPeopleData.setModifiedTime(now);
+
+            peopleDataMapper.insert(tPeopleData);
+
             importedCount++;
             memberCountLabel.setText(String.valueOf(importedCount));
             progressBar.setValue(importedCount);
@@ -275,7 +300,18 @@ public class ImportByWxMp extends JDialog {
             }
             openIds = wxMpUserList.getOpenids();
             for (String openId : openIds) {
-                PushData.allUser.add(new String[]{openId});
+                String[] array = {openId};
+
+                TPeopleData tPeopleData = new TPeopleData();
+                tPeopleData.setPeopleId(PeopleManageListener.selectedPeopleId);
+                tPeopleData.setPin(array[0]);
+                tPeopleData.setVarData(JSONUtil.toJsonStr(array));
+                tPeopleData.setAppVersion(UiConsts.APP_VERSION);
+                tPeopleData.setCreateTime(now);
+                tPeopleData.setModifiedTime(now);
+
+                peopleDataMapper.insert(tPeopleData);
+
                 importedCount++;
                 memberCountLabel.setText(String.valueOf(importedCount));
                 progressBar.setValue(importedCount);
@@ -311,6 +347,8 @@ public class ImportByWxMp extends JDialog {
             return;
         }
 
+        String now = SqliteUtil.nowDateForSqlite();
+
         List<String> openIds = wxTagListUser.getData().getOpenidList();
 
         tagUserSet = Collections.synchronizedSet(new HashSet<>());
@@ -329,12 +367,20 @@ public class ImportByWxMp extends JDialog {
             tagUserSet.addAll(openIds);
         }
 
-        PushData.allUser = Collections.synchronizedList(new ArrayList<>());
         for (String openId : tagUserSet) {
-            PushData.allUser.add(new String[]{openId});
+            String[] array = {openId};
+
+            TPeopleData tPeopleData = new TPeopleData();
+            tPeopleData.setPeopleId(PeopleManageListener.selectedPeopleId);
+            tPeopleData.setPin(array[0]);
+            tPeopleData.setVarData(JSONUtil.toJsonStr(array));
+            tPeopleData.setAppVersion(UiConsts.APP_VERSION);
+            tPeopleData.setCreateTime(now);
+            tPeopleData.setModifiedTime(now);
+
+            peopleDataMapper.insert(tPeopleData);
         }
 
-        memberCountLabel.setText(String.valueOf(PushData.allUser.size()));
         progressBar.setIndeterminate(false);
         progressBar.setValue(progressBar.getMaximum());
 
@@ -365,6 +411,25 @@ public class ImportByWxMp extends JDialog {
 
         if (wxTagListUser.getCount() == 0) {
             return;
+        }
+
+        String now = SqliteUtil.nowDateForSqlite();
+
+        // 保存导入配置
+        TPeopleImportConfig beforePeopleImportConfig = peopleImportConfigMapper.selectByPeopleId(PeopleManageListener.selectedPeopleId);
+
+        TPeopleImportConfig tPeopleImportConfig = new TPeopleImportConfig();
+        tPeopleImportConfig.setPeopleId(PeopleManageListener.selectedPeopleId);
+        tPeopleImportConfig.setLastWay(String.valueOf(PeopleImportWayEnum.BY_NUM_CODE));
+        tPeopleImportConfig.setAppVersion(UiConsts.APP_VERSION);
+        tPeopleImportConfig.setModifiedTime(now);
+
+        if (beforePeopleImportConfig != null) {
+            tPeopleImportConfig.setId(beforePeopleImportConfig.getId());
+            peopleImportConfigMapper.updateByPrimaryKeySelective(tPeopleImportConfig);
+        } else {
+            tPeopleImportConfig.setCreateTime(now);
+            peopleImportConfigMapper.insert(tPeopleImportConfig);
         }
 
         List<String> openIds = wxTagListUser.getData().getOpenidList();
@@ -403,142 +468,23 @@ public class ImportByWxMp extends JDialog {
             }
         }
 
-        PushData.allUser = Collections.synchronizedList(new ArrayList<>());
         for (String openId : tagUserSet) {
-            PushData.allUser.add(new String[]{openId});
+            String[] array = {openId};
+
+            TPeopleData tPeopleData = new TPeopleData();
+            tPeopleData.setPeopleId(PeopleManageListener.selectedPeopleId);
+            tPeopleData.setPin(array[0]);
+            tPeopleData.setVarData(JSONUtil.toJsonStr(array));
+            tPeopleData.setAppVersion(UiConsts.APP_VERSION);
+            tPeopleData.setCreateTime(now);
+            tPeopleData.setModifiedTime(now);
+
+            peopleDataMapper.insert(tPeopleData);
         }
 
-        memberCountLabel.setText(String.valueOf(PushData.allUser.size()));
         progressBar.setIndeterminate(false);
         progressBar.setValue(progressBar.getMaximum());
 
-    }
-
-    /**
-     * 获取导入数据信息列表
-     */
-    public static void renderMemberListTable() {
-        JTable memberListTable = MemberForm.getInstance().getMemberListTable();
-        JProgressBar progressBar = MemberForm.getInstance().getMemberTabImportProgressBar();
-        MemberForm memberForm = MemberForm.getInstance();
-
-        DefaultTableModel tableModel = (DefaultTableModel) memberListTable.getModel();
-        tableModel.setRowCount(0);
-        progressBar.setVisible(true);
-        progressBar.setMaximum(PushData.allUser.size());
-
-        int msgType = App.config.getMsgType();
-
-        // 导入列表
-        List<String> headerNameList = Lists.newArrayList();
-        headerNameList.add("Data");
-        if (MessageTypeEnum.isWxMaOrMpType(msgType)) {
-            if (memberForm.getImportOptionAvatarCheckBox().isSelected()) {
-                headerNameList.add("头像");
-            }
-            if (memberForm.getImportOptionBasicInfoCheckBox().isSelected()) {
-                headerNameList.add("昵称");
-                headerNameList.add("性别");
-                headerNameList.add("地区");
-                headerNameList.add("关注时间");
-            }
-            headerNameList.add("openId");
-        } else {
-            headerNameList.add("数据");
-        }
-
-        String[] headerNames = new String[headerNameList.size()];
-        headerNameList.toArray(headerNames);
-        DefaultTableModel model = new DefaultTableModel(null, headerNames);
-        memberListTable.setModel(model);
-        if (MessageTypeEnum.isWxMaOrMpType(msgType) && memberForm.getImportOptionAvatarCheckBox().isSelected()) {
-            memberListTable.getColumn("头像").setCellRenderer(new TableInCellImageLabelRenderer());
-        }
-
-        DefaultTableCellRenderer hr = (DefaultTableCellRenderer) memberListTable.getTableHeader()
-                .getDefaultRenderer();
-        // 表头列名居左
-        hr.setHorizontalAlignment(DefaultTableCellRenderer.LEFT);
-
-        // 隐藏第0列Data数据列
-        JTableUtil.hideColumn(memberListTable, 0);
-
-        // 设置行高
-        if (MessageTypeEnum.isWxMaOrMpType(msgType) && memberForm.getImportOptionAvatarCheckBox().isSelected()) {
-            memberListTable.setRowHeight(66);
-        } else {
-            memberListTable.setRowHeight(36);
-        }
-
-        List<Object> rowDataList;
-        WxMpService wxMpService = null;
-        boolean needToGetInfoFromWeiXin = false;
-        if (MessageTypeEnum.isWxMaOrMpType(msgType) && (memberForm.getImportOptionBasicInfoCheckBox().isSelected() ||
-                memberForm.getImportOptionAvatarCheckBox().isSelected())) {
-            needToGetInfoFromWeiXin = true;
-        }
-        if (needToGetInfoFromWeiXin) {
-            wxMpService = WxMpTemplateMsgSender.getWxMpService();
-        }
-        for (int i = 0; i < PushData.allUser.size(); i++) {
-            String[] importedData = PushData.allUser.get(i);
-            try {
-                String openId = importedData[0];
-                rowDataList = new ArrayList<>();
-                rowDataList.add(String.join("|", importedData));
-                if (needToGetInfoFromWeiXin) {
-                    WxMpUser wxMpUser = null;
-                    TWxMpUser tWxMpUser = tWxMpUserMapper.selectByPrimaryKey(openId);
-                    if (tWxMpUser != null) {
-                        wxMpUser = new WxMpUser();
-                        BeanUtil.copyProperties(tWxMpUser, wxMpUser);
-                    } else {
-                        if (wxMpService != null) {
-                            try {
-                                wxMpUser = wxMpService.getUserService().userInfo(openId);
-                                if (wxMpUser != null) {
-                                    tWxMpUser = new TWxMpUser();
-                                    BeanUtil.copyProperties(wxMpUser, tWxMpUser);
-                                    tWxMpUserMapper.insertSelective(tWxMpUser);
-                                }
-                            } catch (Exception e) {
-                                logger.error(e);
-                            }
-                        }
-                    }
-
-                    if (wxMpUser != null) {
-                        if (memberForm.getImportOptionAvatarCheckBox().isSelected()) {
-                            rowDataList.add(wxMpUser.getHeadImgUrl());
-                        }
-                        if (memberForm.getImportOptionBasicInfoCheckBox().isSelected()) {
-                            rowDataList.add(wxMpUser.getNickname());
-                            rowDataList.add(wxMpUser.getSexDesc());
-                            rowDataList.add(wxMpUser.getCountry() + "-" + wxMpUser.getProvince() + "-" + wxMpUser.getCity());
-                            rowDataList.add(DateFormatUtils.format(wxMpUser.getSubscribeTime() * 1000, "yyyy-MM-dd HH:mm:ss"));
-                        }
-                    } else {
-                        if (memberForm.getImportOptionAvatarCheckBox().isSelected()) {
-                            rowDataList.add("");
-                        }
-                        if (memberForm.getImportOptionBasicInfoCheckBox().isSelected()) {
-                            rowDataList.add("");
-                            rowDataList.add("");
-                            rowDataList.add("");
-                            rowDataList.add("");
-                        }
-                    }
-                    rowDataList.add(openId);
-                } else {
-                    rowDataList.add(String.join("|", importedData));
-                }
-
-                model.addRow(rowDataList.toArray());
-            } catch (Exception e) {
-                logger.error(e);
-            }
-            progressBar.setValue(i + 1);
-        }
     }
 
     {
