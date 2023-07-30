0f075fb2acbd46a8e0265cf4bad7908e9784ffec
rememberber <rememberber@163.com>
2023-07-30 23:41:05 +0800

    UI细节调整

--- src/main/java/com/fangxuele/tool/push/ui/frame/HttpResultFrame.java
+++ src/main/java/com/fangxuele/tool/push/ui/frame/HttpResultFrame.java
@@ -1,15 +1,10 @@
 package com.fangxuele.tool.push.ui.frame;
 
-import com.apple.eawt.Application;
-import com.fangxuele.tool.push.ui.UiConsts;
 import com.fangxuele.tool.push.ui.form.HttpResultForm;
 import com.fangxuele.tool.push.util.ComponentUtil;
-import com.fangxuele.tool.push.util.SystemUtil;
-import com.google.common.collect.Lists;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
 
 /**
  * <pre>
@@ -29,22 +24,6 @@ public class HttpResultFrame extends JFrame {
         String title = "Http请求结果";
         this.setName(title);
         this.setTitle(title);
-        List<Image> images = Lists.newArrayList();
-        images.add(UiConsts.IMAGE_LOGO_1024);
-        images.add(UiConsts.IMAGE_LOGO_512);
-        images.add(UiConsts.IMAGE_LOGO_256);
-        images.add(UiConsts.IMAGE_LOGO_128);
-        images.add(UiConsts.IMAGE_LOGO_64);
-        images.add(UiConsts.IMAGE_LOGO_48);
-        images.add(UiConsts.IMAGE_LOGO_32);
-        images.add(UiConsts.IMAGE_LOGO_24);
-        images.add(UiConsts.IMAGE_LOGO_16);
-        this.setIconImages(images);
-        // Mac系统Dock图标
-        if (SystemUtil.isMacOs()) {
-            Application application = Application.getApplication();
-            application.setDockIconImage(UiConsts.IMAGE_LOGO_1024);
-        }
 
         ComponentUtil.setPreferSizeAndLocateToCenter(this, 0.6, 0.66);
     }
