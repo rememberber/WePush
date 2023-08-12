3fc51126f3dde0b59da4afa973d998670ac0acf6
rememberber <rememberber@163.com>
2023-08-12 16:13:00 +0800

    opt

--- src/main/java/com/fangxuele/tool/push/ui/form/TaskForm.java
+++ src/main/java/com/fangxuele/tool/push/ui/form/TaskForm.java
@@ -153,7 +153,9 @@ public class TaskForm {
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayoutManager(1, 1, new Insets(10, 10, 10, 10), -1, -1));
         mainSplitPane = new JSplitPane();
+        mainSplitPane.setContinuousLayout(true);
         mainSplitPane.setDividerLocation(516);
+        mainSplitPane.setDoubleBuffered(true);
         mainPanel.add(mainSplitPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
