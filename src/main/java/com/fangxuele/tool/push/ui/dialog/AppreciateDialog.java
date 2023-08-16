c26303064ea18c288fa843f26e37f68c7545996c
rememberber <rememberber@163.com>
2023-08-16 11:13:59 +0800

    opt

--- src/main/java/com/fangxuele/tool/push/ui/dialog/AppreciateDialog.java
+++ src/main/java/com/fangxuele/tool/push/ui/dialog/AppreciateDialog.java
@@ -1,8 +1,6 @@
 package com.fangxuele.tool.push.ui.dialog;
 
 import cn.hutool.core.thread.ThreadUtil;
-import cn.hutool.http.HttpUtil;
-import cn.hutool.json.JSONUtil;
 import com.fangxuele.tool.push.App;
 import com.fangxuele.tool.push.ui.UiConsts;
 import com.fangxuele.tool.push.util.ComponentUtil;
@@ -11,7 +9,6 @@ import com.formdev.flatlaf.util.SystemInfo;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import javax.imageio.ImageIO;
@@ -21,7 +18,6 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Map;
 
 @Slf4j
 public class AppreciateDialog extends JDialog {
@@ -65,19 +61,13 @@ public class AppreciateDialog extends JDialog {
      * 初始化二维码
      */
     public void initQrCode() {
-        // TODO
-        String qrCodeContent = HttpUtil.get(UiConsts.QR_CODE_URL);
-        if (StringUtils.isNotEmpty(qrCodeContent)) {
-            Map<String, String> urlMap = JSONUtil.toBean(qrCodeContent, Map.class);
-            try {
-                URL url = new URL(urlMap.get("url"));
-                BufferedImage image = ImageIO.read(url);
-                qrCodeLabel.setIcon(new ImageIcon(image));
-            } catch (IOException e) {
-                e.printStackTrace();
-                log.error(ExceptionUtils.getStackTrace(e));
-            }
-
+        try {
+            URL url = new URL(UiConsts.QR_CODE_URL);
+            BufferedImage image = ImageIO.read(url);
+            qrCodeLabel.setIcon(new ImageIcon(image));
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(ExceptionUtils.getStackTrace(e));
         }
     }
 
@@ -107,7 +97,7 @@ public class AppreciateDialog extends JDialog {
         panel1.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         contentPane.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         qrCodeLabel = new JLabel();
-        qrCodeLabel.setIcon(new ImageIcon(getClass().getResource("/icon/weixin-qrcode.png")));
+        qrCodeLabel.setIcon(new ImageIcon(getClass().getResource("/icon/wx-zanshang.jpg")));
         qrCodeLabel.setText("");
         qrCodeLabel.setToolTipText("感谢您的鼓励和支持！");
         panel1.add(qrCodeLabel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
