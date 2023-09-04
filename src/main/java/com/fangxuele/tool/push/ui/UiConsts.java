0d3cce3a75c08e9413ad820d1cd2423d26cfb2cd
rememberber <rememberber@163.com>
2023-09-04 17:38:54 +0800

    v_5.0.2

--- src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
+++ src/main/java/com/fangxuele/tool/push/ui/UiConsts.java
@@ -19,7 +19,7 @@ public class UiConsts {
      * 软件名称,版本
      */
     public final static String APP_NAME = "WePush";
-    public final static String APP_VERSION = "v_5.0.1";
+    public final static String APP_VERSION = "v_5.0.2";
 
     public static final int TABLE_ROW_HEIGHT = 30;
 
